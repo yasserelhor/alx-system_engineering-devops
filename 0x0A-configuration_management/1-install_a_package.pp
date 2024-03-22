4a7e7ee651e650c93039953bd0b9e7c65ba4f612
yasserelhor <elhoryasser6@gmail.com>
2024-03-22 21:02:42 +0000

    adding the thing

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-# install puppet version 2.1.0
+# install flas and puppt version 2.1.0
 
-exec { 'puppet-lint':
-  command => '/usr/bin/apt-get -y install puppet-lint -v 2.1.0',
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
