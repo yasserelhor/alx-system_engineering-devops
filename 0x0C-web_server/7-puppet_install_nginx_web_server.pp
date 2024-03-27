d193fe847dbf6f1ca70f00bda09c8d1372870a6c
yasserelhor <elhoryasser6@gmail.com>
2024-03-27 02:51:33 +0000

    adding the thing

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -7,6 +7,7 @@ file_line { 'aaaaa':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://imtheonlyone permanent;',
 }
 
 file { '/var/www/html/index.html':
