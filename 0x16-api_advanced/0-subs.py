0d7a366b658ef6e8f1d6437304386ca16a15f320
yasserelhor <elhoryasser6@gmail.com>
2024-05-07 18:57:31 +0100

    adding the thing

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
-""" script to obtain subscribers
-    count from a subreddit
 """
-from requests import get
+This script queries
+"""
+
+import requests
 
 
 def number_of_subscribers(subreddit):
-    """ function to get subscriber count"""
-    if subreddit and type(subreddit) is str:
-        subscribers = 0
-        url = 'https://reddit.com/r/{}/about.json'.format(subreddit)
-        headers = {'user-agent': 'my-app/0.0.1'}
-        req = get(url, headers=headers)
-        if req.status_code == 200:
-            data = req.json()
-            subscribers = data.get('data', {}).get('subscribers', 0)
+    """return The redist"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
         return subscribers
+    else:
+        return 0
